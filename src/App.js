023495b9483d7b6f730742872a3cfb90559b298a
Corey Noble <coreynoble@hotmail.ca>
2021-10-11 20:08:25 -0400

    RESET

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
-import { useState } from 'react'
-
-import Form from './components/Form/Form'
-import Card from './components/UI/Card/Card'
-import UserList from './components/UserList/UserList'
-
 function App() {
-  const [userList, setUserList] = useState([])
-
-  const addUser = (userData) => {
-    const newUser = {
-      id: Math.random(),
-      name: userData.name,
-      age: userData.age
-    }
-
-    setUserList([
-      ...userList,
-      newUser
-    ])
-  }
-
   return (
     <div>
-      <Card>
-        <Form addUser={addUser} />
-      </Card>
-      {userList.length && (
-        <Card>
-          <UserList users={userList} />
-        </Card>
-      )}
     </div>
   );
 }
